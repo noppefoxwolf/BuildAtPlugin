ae37b311dec10aca43f4e5956420d18cea0ae8e3
noppefoxwolf <noppelabs@gmail.com>
2023-09-14 12:40:49 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildAtPlugin",
+    products: [
+        .plugin(
+            name: "BuildAtPlugin",
+            targets: ["BuildAtPlugin"]),
+    ],
+    targets: [
+        .plugin(
+            name: "BuildAtPlugin",
+            capability: .buildTool()
+        ),
+    ]
+)
---
+++ Plugins/BuildAtPlugin.swift
@@ -0,0 +1,52 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct BuildAtPlugin: BuildToolPlugin {
+    /// Entry point for creating build commands for targets in Swift packages.
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let tmpOutputFilePathString = ""
+        let outputFilePath = Path("")
+        
+        let generatedFileContent = """
+        public extension Date {
+            static let buildAt: Date = Date(timeIntervalSinceReferenceDate: \(Date.now.timeIntervalSinceReferenceDate)
+        }
+        """
+        
+        try generatedFileContent.write(to: URL(fileURLWithPath: tmpOutputFilePathString), atomically: true, encoding: .utf8)
+        
+        return [
+            .prebuildCommand(
+                displayName: "BuildAtPlugin",
+                executable: Path("/bin/cp"),
+                arguments: [
+                    tmpOutputFilePathString,
+                    outputFilePath.string
+                ],
+                outputFilesDirectory: outputFilePath.removingLastComponent()
+            )
+        ]
+    }
+    
+    private let generatedFileName = "BuildAtPlugin+Generated.swift"
+    
+    private func tmpOutputFilePath() throws -> Path {
+        let tmpDirectory = Path(NSTemporaryDirectory())
+        try FileManager.default.createDirectoryIfNotExists(atPath: tmpDirectory.string)
+        return tmpDirectory.appending(generatedFileName)
+    }
+    
+    private func outputFilePath(workDirectory: Path) throws -> Path {
+        let outputDirectory = workDirectory.appending("Output")
+        try FileManager.default.createDirectoryIfNotExists(atPath: outputDirectory.string)
+        return outputDirectory.appending(generatedFileName)
+    }
+}
+
+extension FileManager {
+    func createDirectoryIfNotExists(atPath path: String) throws {
+        guard !fileExists(atPath: path) else { return }
+        try createDirectory(atPath: path, withIntermediateDirectories: true)
+    }
+}
