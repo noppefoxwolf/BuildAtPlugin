7e72dedd0e4f30ad24720527015f14e0c369aec4
noppefoxwolf <noppelabs@gmail.com>
2023-09-14 13:11:18 +0900

    Update BuildAtPlugin.swift

--- Plugins/BuildAtPlugin/BuildAtPlugin.swift
+++ Plugins/BuildAtPlugin/BuildAtPlugin.swift
@@ -8,6 +8,8 @@ struct BuildAtPlugin: BuildToolPlugin {
         let outputFilePath = try outputFilePath(workDirectory: context.pluginWorkDirectory)
         
         let generatedFileContent = """
+        import Foundation
+        
         public extension Date {
             static let buildAt: Date = Date(timeIntervalSinceReferenceDate: \(Date.now.timeIntervalSinceReferenceDate))
         }
